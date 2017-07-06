97e93043b547bb30d0dc00808c7425937b9d17b9
Etgar Shmueli <etgar90@gmail.com>
2017-07-06 17:12:29 +0300

    Viber Bot authentication TOKEN

---
+++ bot.py
@@ -0,0 +1,16 @@
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from flask import Flask, request, Response
+
+bot_configuration = BotConfiguration(
+	name='InvestBot',
+	avatar='http://viber.com/avatar.jpg',
+	auth_token='4643793ea227d275-7a894dbfc47308e7-8ba014f6e7d19fa8'
+)
+
+
+def main():
+    viber = Api(bot_configuration)
+
+if __name__ == "__main__":
+    main()
