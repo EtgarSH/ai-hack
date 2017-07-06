edd21a22f97d062533fee869682c29dcdcd3f641
Etgar Shmueli <etgar90@gmail.com>
2017-07-06 17:51:46 +0300

    Echo Action

---
+++ actions/echo.py
@@ -0,0 +1,40 @@
+from flask import Response
+
+from viberbot.api.messages import (
+    TextMessage,
+    ContactMessage,
+    PictureMessage,
+    VideoMessage
+)
+from viberbot.api.messages.data_types.contact import Contact
+
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+
+
+def echo(request, viber, logger):
+    logger.debug("received request. post data: {0}".format(request.get_data()))
+    # every viber message is signed, you can verify the signature using this method
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
+
+    # this library supplies a simple way to receive a request object
+    viber_request = viber.parse_request(request.get_data())
+
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+    return Response(status=200)
